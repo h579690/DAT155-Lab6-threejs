23326afa66dba13de66676a2751e10170640649c
h579690 <h579690@stud.hvl.no>
2020-11-17 23:01:20 +0100

    Egen klasse for skyer

--- Dat155-Lab6/js/main.js
+++ Dat155-Lab6/js/main.js
@@ -212,47 +212,11 @@ async function main() {
     /**
      * Add clouds
      */
-    //let cloud = new Cloud();
-    //scene.add(cloud);
-    function generateBillboardClouds() {
-        let loader = new TextureLoader();
-        for (let i = 0; i < 20; i++) {
-            let cloudTextures = [
-                loader.load('resources/cloud/cloud1.png'), //Laster inn noen skyteksturer
-                loader.load('resources/cloud/cloud2.png'),
-                loader.load('resources/cloud/cloud3.png'),
-                loader.load('resources/cloud/cloud4.png')
-            ];
+        //let cloud = new Cloud();
+        //scene.add(cloud);
+    let Clouds = new Cloud(scene);
 
-
-            //let random = Math.floor(Math.random() * 4);
-            let material = new SpriteMaterial({
-                map: cloudTextures[3],
-                color: 0xffffff,
-                transparent: true,
-                opacity: 3.0,
-                side: DoubleSide
-            });
-
-            let skyPlane = new Sprite(material);
-
-            //Positions- plasser litt tilfeldig
-            let pX = Math.random() * 1000 - 500;
-            let pZ = Math.random() * 1000 - 500;
-            let pY = Math.random() * 50 + 100;
-
-            let s1 = 50;
-            let s2 = 50;
-
-            //Set positions and scale
-            skyPlane.position.set(pX, pY, pZ);
-            skyPlane.scale.set(s1, s2, 1);
-            skyPlane.rotation.z = Math.PI / 2;
-
-            scene.add(skyPlane);
-        }
-    }
-    generateBillboardClouds();
+    Clouds.generateBillboardClouds();
 
 
     /**
--- Dat155-Lab6/js/terrain/Cloud.js
+++ Dat155-Lab6/js/terrain/Cloud.js
@@ -1,53 +1,55 @@
-import {Object3D, Sprite, SpriteMaterial, DoubleSide, TextureLoader} from "../lib/three.module.js";
+import {
+    Sprite,
+    SpriteMaterial,
+    DoubleSide,
+    TextureLoader
+} from "../lib/three.module.js";
 
-export default class Cloud extends Object3D {
-    constructor() {
-        super();
-
-        this.add(generateBillboardClouds());
+export default class Cloud {
+    constructor(scene) {
+        this.scene = scene;
     }
-}
 
-function generateBillboardClouds() {
-    let loader = new TextureLoader();
-    for (let i = 0; i < 20; i++) {
-        /*var cloudtextures = [
-            loader.load('resources/cloud/c1.jpg'), //Laster inn noen skyteksturer
-            loader.load('resources/cloud/c2.jpg'),
-            loader.load('resources/cloud/c3.jpg'),
-            loader.load('resources/cloud/c4.jpg'),
-            loader.load('resources/cloud/c5.jpg')
-        ];*/
+    generateBillboardClouds() {
+        const loader = new TextureLoader();
+        for (let i = 0; i < 30; i++) {
+            let cloudTextures = [
+                loader.load('resources/cloud/cloud1.png'), //Laster inn noen skyteksturer
+                loader.load('resources/cloud/cloud2.png'),
+                loader.load('resources/cloud/cloud3.png'),
+                loader.load('resources/cloud/cloud4.png')
+            ];
 
-        let cloudTexture = loader.load('resources/cloud/cloud.png');
+            //let random = Math.floor(Math.random() * 4);
+            let material = new SpriteMaterial({
+                map: cloudTextures[3],
+                color: 0xffffff,
+                transparent: true,
+                opacity: 3.0,
+                side: DoubleSide
+            });
 
-        //let randomTexture = Math.floor(Math.random() * 4);
-        let material = new SpriteMaterial({
-            map: cloudTexture,
-            color: 0xffffff,
-            fog: true,
-            transparent: true,
-            opacity: 3.0,
-            side: DoubleSide
-        });
-        let skyPlane = new Sprite(material);
+            let skyPlane = new Sprite(material);
 
-        //Positions- plasser litt tilfeldig
-        let pX = Math.random() * 1000 - 500;
-        let pZ = Math.random() * 1000 - 500;
-        let pY = Math.random() * 50 + 100;
-        if (i < 2) {
-            pX = 185;
-            pY = 100;
-            pZ = 185;
+            //Positions- plasser litt tilfeldig
+            let pX = Math.random() * 1000 - 500;
+            let pZ = Math.random() * 1000 - 500;
+            let pY = Math.random() * 50 + 100;
+            if (i < 2) {
+                pX = 185;
+                pY = 100;
+                pZ = 185;
+            }
+            let s1 = 50;
+            let s2 = 50;
+
+            //Set positions and scale
+            skyPlane.position.set(pX, pY, pZ);
+            skyPlane.scale.set(s1, s2, 1);
+            skyPlane.rotation.z = Math.PI / 2;
+
+
+            this.scene.add(skyPlane);
         }
-        let s1 = 50;
-        let s2 = 50;
-
-        //Set positions and scale
-        skyPlane.position.set(pX, pY, pZ);
-        skyPlane.scale.set(s1, s2, 1);
-        skyPlane.rotation.z = Math.PI / 2;
-
     }
 }
